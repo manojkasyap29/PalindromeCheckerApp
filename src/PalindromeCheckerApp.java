c5b96fa49e73045ddf90b640c2727fa5864388c6
manojkasyap29 <manojkasyap2903@gmail.com>
2026-02-18 15:09:24 +0530

    UC2: Hardcoded palindrome result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,14 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
         System.out.println("Palindrome Checker App");
         System.out.println("Version: 1.0");
         System.out.println("-----------------------------");
+
+
+        String input = "madam";
+        if (input.equals("madam")) {
+            System.out.println("UC2: madam is a palindrome (Hardcoded)");
+        }
     }
 }
